5ae419e10ae03827f70d6c1263ee0457ea28623d
Bartek P <bartekpretnik@gmail.com>
2023-02-18 15:36:23 +0100

    initial

---
+++ src/main/java/pl/go/volley/govolley/GoVolleyApplication.java
@@ -0,0 +1,13 @@
+package pl.go.volley.govolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoVolleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoVolleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/go/volley/govolley/GoVolleyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.go.volley.govolley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoVolleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
